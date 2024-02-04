36872c9f0144739d47714c743b082a6d384c1eba
domiFar <135108965+domiFar@users.noreply.github.com>
2024-02-04 15:31:18 +0100

    Update LoginPage.js

--- public/Logic/LoginPage.js
+++ public/Logic/LoginPage.js
@@ -1,18 +1,1 @@
-//import { Database } from "./DatabaseHandler.js"
-
-const fs = require('node:fs')
-
-function createAccount() {
-    fs.appendFile('./Database/Users.txt', content(document.querySelector("#nameInput"), document.querySelector("#passwordInput"), 0), err => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log('Wrote into Database')
-        }
-      });
-    // let d = new Database();
-    // d.WriteIntoDatabase(document.querySelector("#nameInput"), document.querySelector("#passwordInput"), 0)
-}
-const content = (name, password, money) => {
-    return name + password + money + ","
-}
+abc
