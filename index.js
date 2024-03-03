bc3fbaf79ee4625918d1191d6876ed69a0c19bfa
domiFar <135108965+domiFar@users.noreply.github.com>
2024-03-03 18:00:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('node:fs')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 80
 
 app.use(express.static('public'))
 app.use(express.json())
